22675276db80e6b98b33457e6ddab38635d691fe
rladmsdk <76425689+rladmsdk@users.noreply.github.com>
2020-12-22 03:39:52 +0900

    A+B

--- 1000.java
+++ 1000.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		int A = in.nextInt();
 		int B = in.nextInt();
