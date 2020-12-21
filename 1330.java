824e880faf851919a136bbb07d01e4a4b62cb7b6
keasilver46 <keasilver46@gmail.com>
2020-12-22 04:09:13 +0900

    두 수 비교하기

---
+++ 1330.java
@@ -0,0 +1,21 @@
+package _1330;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		if(A > B)
+			System.out.println(">");
+		else if(A < B)
+			System.out.println("<");
+		else
+			System.out.println("==");
+	}
+
+}
---
+++ 2884.java
@@ -0,0 +1,31 @@
+package _2884;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int H = in.nextInt();
+		int M = in.nextInt();
+		int hour, minute;
+		
+		if (H>0 && M<45) {
+			hour = H-1;
+			minute = (M+60)-45;
+		} 
+		else if (H == 0 && M<45) {
+			hour = 23;
+			minute = (M+60)-45;
+		}
+		else {
+			hour = H;
+			minute = M-45;
+		}
+		
+		System.out.printf("%d %d", hour, minute);
+				
+	}
+
+}
