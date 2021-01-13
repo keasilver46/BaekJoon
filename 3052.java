ad44db18dfb7970acf71c8c356ae40154ac8246f
keasilver46 <76425689+keasilver46@users.noreply.github.com>
2021-01-13 16:43:31 +0900

    나머지

--- 3052.java
+++ 3052.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		int num[] = new int[10];
 		int result[] = new int[42];
