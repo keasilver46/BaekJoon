05ee81b0143fba6d2b5c1574ba16211b9c77d535
keasilver46 <keasilver46@gmail.com>
2022-06-18 02:42:29 +0900

    오름차순 정렬

---
+++ 2750.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {	
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int [] arr = new int[N];
+		int tmp;
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = i+1; j < N; j++) {
+				if (arr[i] > arr[j]) {
+					tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+	
+}
