8daf376c0e327d23420ca2fd1f49598100cad318
keasilver46 <keasilver46@gmail.com>
2022-06-18 03:24:39 +0900

    카운팅 정렬

---
+++ 10989.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception{	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[10001];
+		
+		for (int i = 0; i < N; i++) {
+			arr[Integer.parseInt(br.readLine())]++;
+		}
+		
+		for (int i = 0; i < 10001; i++) {
+			while (arr[i] > 0) {
+				sb.append(i).append('\n');
+				arr[i]--; 
+			}
+		}
+		
+		System.out.println(sb);
+	}
+	
+}
