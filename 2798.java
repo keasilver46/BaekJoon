25709602dfb8584057a8f0bc4a93f1a7eca4d045
keasilver46 <keasilver46@gmail.com>
2022-06-18 02:03:12 +0900

    블랙잭

---
+++ 2798.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {	
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int [] arr = new int[N];
+		int tmp;
+		int sum = 0;
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < N-2; i++) {
+			for (int j = i+1; j < N-1; j++) {
+				for (int k = j+1; k < N; k++) {
+					tmp = arr[i] + arr[j] + arr[k];
+					
+					if (tmp > sum && tmp <= M) {
+						sum = tmp;
+					}
+				}
+			}
+		}
+		
+		System.out.println(sum);
+	}
+	
+}
