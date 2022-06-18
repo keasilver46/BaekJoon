a03610cf84c651f81e17cd0bdc5510ad131ab750
keasilver46 <keasilver46@gmail.com>
2022-06-19 01:24:57 +0900

    Collections.sort

---
+++ 2751.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception{	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(list);
+		
+		for (int num : list) {
+			sb.append(num).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+	
+}
