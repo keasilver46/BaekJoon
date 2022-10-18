363a8c400bfb7fdbf7d1216c686894c560468578
keasilver46 <keasilver46@gmail.com>
2022-10-18 15:43:22 +0900

    골드바흐의 추측

---
+++ 6588.py
@@ -0,0 +1,17 @@
+isPrime = [True for i in range(1000001)]
+
+for i in range(2, 1000001):
+  if isPrime[i]:
+    for j in range(i*2, 1000001, i):
+      isPrime[j] = False
+
+while True:
+  n = int(input())
+
+  if n == 0:
+    break
+  else:
+    for i in range(3, len(isPrime)):
+      if isPrime[i] & isPrime[n-i]:
+        print(n, '=', i, '+', n-i)
+        break
