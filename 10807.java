205380c29849bb486f8af3e685d288fbd66bf0b6
Euna Kim <kesilver46@gmai.com>
2022-11-20 23:11:42 +0900

    개수 세기

---
+++ 10807.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int v = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int count = 0;
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			if (arr[i] == v) {
+				count += 1;
+			}
+		}
+		
+		System.out.println(count);
+	}
+	
+}
