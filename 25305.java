310fe28ca29080aaa01773b7eba06e9d3e74f8d7
Euna Kim <kesilver46@gmai.com>
2023-01-30 21:57:40 +0900

    커트라인

---
+++ 25305.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int k = sc.nextInt();
+		int[] arr = new int[N];
+		int tmp;
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (arr[i] < arr[j]) {
+					tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+		}
+		
+		System.out.println(arr[k-1]);
+	}
+	
+}
