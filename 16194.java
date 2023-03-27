45dd6a542634581fca7aafa9261629ad29498b82
Euna Kim <kesilver46@gmai.com>
2023-03-27 20:50:56 +0900

    카드 구매하기

---
+++ 16194.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] P = new int[N+1];
+		int[] D = new int[N+1];
+		for (int i = 1; i <= N; i++) {
+			P[i] = Integer.parseInt(st.nextToken());
+			D[i] = Integer.MAX_VALUE;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++) {
+				D[i] = Math.min(D[i], P[j] + D[i-j]);
+			}
+		}
+		
+		System.out.println(D[N]);
+	}
+
+}
